776e57b452b3fc158d225d639e4c9a8fb49750d0
TheBarzani <ismaelmergasori@gmail.com>
2024-12-01 20:40:10 -0500

    dockered the system with rabbitmq (kinda)

--- user_service_v1/app/events.py
+++ user_service_v1/app/events.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-QUEUE_NAME = os.getenv('QUEUE_NAME')
+QUEUE_NAME = os.getenv('RABBITMQ_QUEUE_NAME')
 
 def publish_user_update_event(user_id, email, address):
     channel, connection = create_channel(QUEUE_NAME)
